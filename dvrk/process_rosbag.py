dd3ea1b512418079ec666883afffc6e73f7a44cc
jlimkg <jlim.kg@gmail.com>
2024-09-12 14:55:28 -0700

    Update process_rosbag.py

--- dvrk/process_rosbag.py
+++ dvrk/process_rosbag.py
@@ -7,10 +7,18 @@ types = []
 for val in bag.get_type_and_topic_info()[1].values():
 	types.append(val[0])
 
-i = 0 
+first = True
+t0 = 0
 for topic, msg, t in bag.read_messages(topics=['/dvrk/PSM1/wrench_body_current', '/dvrk/PSM2/wrench_body_current']):
-	if i==2: break
-	print(topic)
+	if first:
+		t0 = msg.header.stamp.secs + msg.header.stamp.nsecs * 1e-9
+		first = False
+	whichPSM = topic.strip("/dvrk/wrench_body_current")
 	print(msg.wrench.force)
-	i+=1
+	fx = msg.wrench.force.x
+	fy = msg.wrench.force.y
+	fz = msg.wrench.force.z
+	t = msg.header.stamp.secs + msg.header.stamp.nsecs * 1e-9 - t0
+	# if topic == '/dvrk/PSM1/wrench_body_current':
+		
 	
