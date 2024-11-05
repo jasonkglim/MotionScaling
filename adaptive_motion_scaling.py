ebbc48a89c439e6d48a486fa381514a706f7cf98
jlimkg <jlim.kg@gmail.com>
2024-11-05 10:14:01 -0800

    Update adaptive_motion_scaling.py

--- adaptive_motion_scaling.py
+++ adaptive_motion_scaling.py
@@ -1,5 +1,5 @@
 # Example of Adaptive Motion Scaling algorithm
-from performance_modeling.performance_models import BayesRegressionPerformanceModel
+from performance_modeling.performance_models import BayesRegressionPerformanceModel, generate_user_model
 from delay_modeling.distribution_estimation import OnlineHistogram
 import pandas as pd
 import deque
@@ -18,19 +18,31 @@ metric = "time_score" # Choose metric to evaluate
 X = data[['latency', 'scale']]
 Y = data[metric]
 
+# Generate user performance model
 user_performance_model = BayesRegressionPerformanceModel(X, Y)
+
+# For estimating the delay distribution
 delay_estimator = OnlineHistogram()
 
-buffer = deque()
 
 # Initialize adaptive motion scaling
-while True:
-    packet = receive_packet() # receive new packet
+buffer = deque()
+while running:
+
+    new_message = receive_message() # receive new packet
     current_delay = get_current_time() - packet.timestamp # get current delay
+
+    # Update current estimation of delay, get desired effective delay
     delay_estimator.update(current_delay) # Update current estimation of latency distribution
     desired_effective_delay = delay_estimator.get_value_at_percentile(90) # get desired effective delay
-    set_scale = user_performance_model.get_optimal_scale(desired_effective_delay) # get optimal scale from user model
+
+    # Get optimal scale from user model
+    set_scale = user_performance_model.get_optimal_scale(desired_effective_delay)
+
+    # Inject delay to achieve desired effective delay
     inject_delay = desired_effective_delay - current_delay # calculate delay to inject
-    buffer.append(packet) # Add packet to command buffer
-    while( (get_current_time() - buffer[-1].timestamp) < inject_delay):
+    buffer.append(message) # Add message to buffer
+
+    # Publish messages while injected delay is not reached
+    while( (get_current_time() - buffer[-1].timestamp) > inject_delay):
         publish(buffer.popleft())
